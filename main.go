4f070e46edb901cc51e68cb1363dd94e35d8153b
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-28 12:41:40 +0800

    feat: support validate command.

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
 	"os"
 
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
@@ -19,10 +20,35 @@ func main() {
 	app.Usage = "packer plugin"
 	app.Action = run
 	app.Flags = []cli.Flag{
+		cli.StringSliceFlag{
+			Name:   "actions",
+			Usage:  "a list of actions to have packer perform",
+			EnvVar: "PLUGIN_ACTIONS",
+		},
 		cli.StringFlag{
-			Name:   "token",
-			Usage:  "telegram token",
-			EnvVar: "PLUGIN_TOKEN,TELEGRAM_TOKEN",
+			Name:   "vars",
+			Usage:  "a map of variables to pass to the Packer `build` commands. Each value is passed as a `<key>=<value>` option",
+			EnvVar: "PLUGIN_VARS",
+		},
+		cli.StringSliceFlag{
+			Name:   "var_files",
+			Usage:  "a list of var files to use. Each value is passed as -var-file=<value>",
+			EnvVar: "PLUGIN_VAR_FILES",
+		},
+		cli.StringSliceFlag{
+			Name:   "except",
+			Usage:  "validate or build all builds other than these",
+			EnvVar: "PLUGIN_EXCEPT",
+		},
+		cli.StringSliceFlag{
+			Name:   "only",
+			Usage:  "validate or build only the specified builds",
+			EnvVar: "PLUGIN_ONLY",
+		},
+		cli.StringFlag{
+			Name:   "template",
+			Usage:  "Will execute multiple builds in parallel as defined in the template",
+			EnvVar: "PLUGIN_TEMPLATE",
 		},
 	}
 
@@ -33,12 +59,38 @@ func main() {
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
 
 func run(c *cli.Context) error {
-	plugin := Plugin{}
+	var version string
+	if BuildNum != "" {
+		version = Version + "+" + BuildNum
+	} else {
+		version = Version
+	}
+	logrus.WithFields(logrus.Fields{
+		"revision": version,
+	}).Info("Drone Packer Plugin Version")
+
+	var vars map[string]string
+	if c.String("vars") != "" {
+		if err := json.Unmarshal([]byte(c.String("vars")), &vars); err != nil {
+			logrus.Panic(err)
+		}
+	}
+
+	plugin := Plugin{
+		Config: Config{
+			Actions:  c.StringSlice("actions"),
+			Vars:     vars,
+			Template: c.String("template"),
+			VarFiles: c.StringSlice("var_files"),
+			Except:   c.StringSlice("except"),
+			Only:     c.StringSlice("only"),
+		},
+	}
 
 	return plugin.Exec()
 }
--- plugin.go
+++ plugin.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
 type (
 	// Config for the plugin.
 	Config struct {
+		Actions    []string
+		Vars       map[string]string
+		Template   string
+		VarFiles   []string
+		SyntaxOnly bool
+		Except     []string
+		Only       []string
 	}
 
 	// Plugin values
@@ -11,7 +28,75 @@ type (
 	}
 )
 
+func pkValidate(config Config) *exec.Cmd {
+	args := []string{
+		"validate",
+	}
+
+	for _, v := range config.VarFiles {
+		args = append(args, "-var-file", fmt.Sprintf("%s", v))
+	}
+
+	for k, v := range config.Vars {
+		args = append(args, "-var")
+		args = append(args, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	if len(config.Except) > 0 {
+		args = append(args, "-except="+strings.Join(config.Except, ","))
+	}
+
+	if len(config.Only) > 0 {
+		args = append(args, "-only="+strings.Join(config.Only, ","))
+	}
+
+	if config.SyntaxOnly {
+		args = append(args, "-syntax-only")
+	}
+
+	args = append(args, config.Template)
+	return exec.Command(
+		"packer",
+		args...,
+	)
+}
+
 // Exec executes the plugin.
 func (p *Plugin) Exec() error {
+	var commands []*exec.Cmd
+	if p.Config.Template == "" {
+		return errors.New("you must provide a template file")
+	}
+
+	if len(p.Config.Actions) == 0 {
+		return errors.New("you must provide packer action")
+	}
+
+	commands = append(commands, exec.Command("packer", "version"))
+
+	logrus.Println(p.Config.Actions)
+	// Add commands listed from Actions
+	for _, action := range p.Config.Actions {
+		switch action {
+		case "validate":
+			commands = append(commands, pkValidate(p.Config))
+		default:
+			return fmt.Errorf("valid actions are: validate, build  You provided %s", action)
+		}
+	}
+
+	for _, c := range commands {
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+
+		err := c.Run()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Fatal("Failed to execute a command")
+		}
+		logrus.Debug("Command completed successfully")
+	}
+
 	return nil
 }
