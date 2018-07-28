f9d6c1b1e91878c0dbe59aaea35a7b71de121742
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-28 16:29:45 +0800

    feat: Parallel testing

--- plugin_test.go
+++ plugin_test.go
@@ -55,6 +55,7 @@ func Test_pkValidate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			if got := pkValidate(tt.args.config); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("pkValidate() = %v, want %v", got, tt.want)
 			}
